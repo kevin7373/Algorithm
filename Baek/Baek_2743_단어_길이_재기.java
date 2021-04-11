f0073caed303ef7b972983ee92da8b5a54fc3a6a
박정주 <kevin7373@naver.com>
2021-04-11 21:52:15 +0900

    feat Baek

---
+++ Baek/Baek_2743_단어_길이_재기.java
@@ -0,0 +1,21 @@
+package day210411;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * Author   : 박정주 
+ * Date     : 2021-04-11
+ * Category : 문자열
+ * Level    : Bronze II
+ * URL      : https://www.acmicpc.net/problem/2743
+ */
+public class Baek_2743_단어_길이_재기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+
+		System.out.println(str.length());
+	}
+}
